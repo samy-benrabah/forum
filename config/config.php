cf9c8d32596a64aed342cc41112074d5d71a3ce9
samy <samy>
2021-01-15 13:19:53 +0100

    ajout fichier samy et nouvelle architecture dossier forum

--- config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$pdo = new PDO('mysql:dbname=forum;host=localhost',"root","")
-
-?>
--- pages/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-setcookie('mail',null, time() -3600, '/');
-session_start();
-session_destroy();
-header('location: ../index.php');
-
-
-?>
